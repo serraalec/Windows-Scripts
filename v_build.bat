4173b7712a9a6b180cbf15f7e711249b36203581
Alec Serra <41804566+serraalec@users.noreply.github.com>
2020-06-06 20:46:41 -0700

    Update v_build.bat

--- v_build.bat
+++ v_build.bat
@@ -1,4 +1,14 @@
 @echo off
+
+goto comment
+/* ========================================================================
+$File: v_build.bat$
+$Date: 6/6/2020$
+$Revision: 1$
+$Creator: Alec Serra $
+    ======================================================================== */
+:comment
+
 set /P name="File to sim? "
 tttbb %name%
 iverilog -o  %name%_tb.vvp %name%_tb.v
